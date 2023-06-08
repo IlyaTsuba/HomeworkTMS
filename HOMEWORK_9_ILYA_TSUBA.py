aa3366ee7e2b076c5440ff4d6617be8feb5844b0
Ilya Tsuba <ilyatsubablr@gmail.com>
2023-06-08 17:50:17 +0300

    Initialize + adding 1st task

---
+++ HOMEWORK_9_ILYA_TSUBA.py
@@ -0,0 +1,13 @@
+class Soda:
+    def __init__(self, taste=None):
+        self.taste = taste
+
+    def __str__(self):
+        if self.taste:
+            return f"You have soda with {self.taste} taste."
+        else:
+            return "You have soda with no taste."
+
+
+my_soda = Soda()
+print(my_soda)
