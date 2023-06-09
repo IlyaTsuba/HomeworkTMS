7da56adfa3b74d9d2104afad6b2783c21e5652f9
Ilya Tsuba <ilyatsubablr@gmail.com>
2023-06-09 12:34:57 +0300

    adding 3rd task

--- HOMEWORK_9_ILYA_TSUBA.py
+++ HOMEWORK_9_ILYA_TSUBA.py
@@ -31,3 +31,37 @@ class Math:
 
 obj2 = Math()
 print(obj2.addition(2, 3))
+
+class Car:
+
+    def __init__(self, color=None, type=None, year=None):
+        self.color = color
+        self.type = type
+        self.year = year
+
+    def start_engine(self):
+        print("Car is started!")
+
+    def turn_off_car(self):
+        print("Car is turned off!")
+
+    def set_color(self, color):
+        self.color = color
+
+    def set_type(self, type):
+        self.type = type
+
+    def set_year(self, year):
+        self.year = year
+
+
+car1 = Car()
+car1.start_engine()
+car1.turn_off_car()
+car1.set_color("blue")
+car1.set_year(2015)
+car1.set_type("bus")
+
+print(car1.color)
+print(car1.year)
+print(car1.type)
